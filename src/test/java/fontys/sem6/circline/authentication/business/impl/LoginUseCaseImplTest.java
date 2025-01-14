fd97b7b7868f98a5143fc38d8aa83111bbdf1420
Sallyphaann <92030592+Sallyphaann@users.noreply.github.com>
2025-01-14 01:05:35 +0100

    integration test

--- src/test/java/fontys/sem6/circline/authentication/business/impl/LoginUseCaseImplTest.java
+++ src/test/java/fontys/sem6/circline/authentication/business/impl/LoginUseCaseImplTest.java
@@ -14,8 +14,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.time.Instant;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
